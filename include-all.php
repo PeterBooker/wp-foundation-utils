bee6c16d4a6782b52a4a20059b36fcc6deff2e3e
Peter Booker <mail@peterbooker.com>
2018-06-29 16:37:15 +0100

    add include all file to allow using all parts together as a plugin

---
+++ include-all.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Foundation Utils
+ * Plugin URI:  https://github.com/PeterBooker/wp-foundation-utils
+ * Description: Range of Classes and Function to aid using Foundation for Sites (v6) with WordPress.
+ * Version:     1.0
+ * Author:      Peter Booker
+ * Author URI:  https://www.peterbooker.com/
+ * License:     GPLv3
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'We\'re sorry, but you can not directly access this file.' );
+}
+
+require_once( dirname( __FILE__ ) . '/menu-walkers/foundation-top-bar.php' );
